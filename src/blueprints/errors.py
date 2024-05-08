d1098e4275ac1df651db0c54b195423c5709b27d
Joao Felipe <joao.felipe.alves.m@gmail.com>
2024-05-08 16:52:56 -0300

    Errors Polished

--- src/blueprints/errors.py
+++ src/blueprints/errors.py
@@ -7,25 +7,25 @@ from src.extensions import af
 errors = Blueprint('/errors', __name__)
 
 
-@errors.errorhandler(IntegrityError)
+@errors.app_errorhandler(IntegrityError)
 def sqlalchemy_integrity_error(error):
     return {
-        'code': error.code,
-        'message': error.name,
-        'description': error.description
-    }, error.code
+        'code': 400,
+        'message': 'Database error',
+        'description': str(error.orig)
+    }, 400
 
 
-@errors.errorhandler(SQLAlchemyError)
+@errors.app_errorhandler(SQLAlchemyError)
 def sqlalchemy_sql_alchemy_error(error):
     return {
-        'code': error.code,
-        'message': error.name,
-        'description': error.description
-    }, error.code
+        'code': InternalServerError.code,
+        'message': InternalServerError().name,
+        'description': InternalServerError.description
+    }, 500
 
 
-@errors.errorhandler(HTTPException)
+@errors.app_errorhandler(HTTPException)
 def werkzeug_http_exception(error):
     return {
         'code': error.code,
@@ -34,20 +34,11 @@ def werkzeug_http_exception(error):
     }, error.code
 
 
-@errors.errorhandler(InternalServerError)
-def werkzeug_internal_server_error(error):
-    return {
-        'code': error.code,
-        'message': error.name,
-        'description': error.description
-    }, error.code
-
-
-@af.error_handler()
-def validation_error(code, messages): # pragma: no cover
+@af.error_handler
+def validation_error(code, messages):
     return {
         'code': code,
         'message': 'Validation Error',
-        'description': ('The found one or more errors in the information that you sent.'),
+        'description': 'The found one or more errors in the information that you sent.',
         'errors': messages
     }, code
