6d7e324ef5b7ed5ef6c452e08e7cf8206b439ad3
Joao Felipe <joao.felipe.alves.m@gmail.com>
2024-05-22 20:33:34 -0300

    Refactor verify_password user query

--- src/auth.py
+++ src/auth.py
@@ -1,6 +1,5 @@
 from flask_httpauth import HTTPBasicAuth, HTTPTokenAuth
 
-from .extensions import db
 from .models import User
 
 basic_auth = HTTPBasicAuth()
@@ -10,7 +9,7 @@ token_auth = HTTPTokenAuth()
 @basic_auth.verify_password
 def verify_password(username, password):
     if username and password:
-        user = db.session.scalar(User.query.filter_by(username=username))
+        user = User.query.filter_by(username=username).scalar()
         if user and user.verify_password(password):
             return user
 
